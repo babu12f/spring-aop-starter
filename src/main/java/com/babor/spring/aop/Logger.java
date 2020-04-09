aae340376ff1afb79cf28068b4089620cdfae469
Shofiullah Babor <babu_12f@yahoo.com>
2020-04-09 21:06:56 +0600

    basic aspect run successfully -75

--- src/main/java/com/babor/spring/aop/Logger.java
+++ src/main/java/com/babor/spring/aop/Logger.java
@@ -1,6 +1,13 @@
 package com.babor.spring.aop;
 
+/***
+ * This Logger Class is an Aspect
+ * */
 public class Logger {
+
+    /**
+     * This method is an Advice
+     * */
     public void aboutToTakePhoto() {
         System.out.println("About to take photo ....");
     }
