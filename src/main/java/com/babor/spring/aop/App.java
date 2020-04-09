d2226796b2217b2d4a973fa7bc9b57c5b7843702
Shofiullah Babor <babu_12f@yahoo.com>
2020-04-09 20:44:03 +0600

    setup spring aop project using maven arctype: quickstart -74

---
+++ src/main/java/com/babor/spring/aop/App.java
@@ -0,0 +1,20 @@
+package com.babor.spring.aop;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        Camera camera = (Camera) context.getBean("camera");
+
+        camera.snap();
+
+        context.close();
+    }
+}
