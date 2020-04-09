6fc59f0049ea6d7bcf1bd0c76cd10cf5c0816326
Shofiullah Babor <babu_12f@yahoo.com>
2020-04-09 21:26:49 +0600

    annotation base aspect -76

--- src/main/java/com/babor/spring/aop/Logger.java
+++ src/main/java/com/babor/spring/aop/Logger.java
@@ -1,14 +1,25 @@
 package com.babor.spring.aop;
 
-/***
- * This Logger Class is an Aspect
- * */
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class Logger {
 
-    /**
-     * This method is an Advice
-     * */
+    @Pointcut("execution(void com.babor.spring.aop.Camera.snap())")
+    public void onCameraSnap() {
+    }
+
+    @Before("onCameraSnap()")
     public void aboutToTakePhoto() {
         System.out.println("About to take photo ....");
+    }
+
+    @Before("onCameraSnap()")
+    public void anotherToTakePhoto() {
+        System.out.println("Before take phone ====================== ");
     }
 }
