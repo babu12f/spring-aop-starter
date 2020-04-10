f8edead1ce19c7925a6928913acf1fada483e6ed
Shofiullah Babor <babu_12f@yahoo.com>
2020-04-10 12:26:26 +0600

    bean PCD -84

--- src/main/java/com/babor/spring/aop/Logger.java
+++ src/main/java/com/babor/spring/aop/Logger.java
@@ -9,34 +9,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class Logger {
 
-//    @Pointcut("within(@Deprecated com.babor.spring..*)")
-//    public void withinPointCut() {
-//    }
-//
-//    @Before("withinPointCut()")
-//    public void withinBefore() {
-//        System.out.println("********** BEFORE ADVICE ************");
-//    }
-
-//    @Pointcut("@target(org.springframework.stereotype.Component)")
-//    public void withinPointCut() {
-//    }
-//
-//    @Before("withinPointCut()")
-//    public void withinBefore() {
-//        System.out.println("********** BEFORE ADVICE ************");
-//    }
-
-//    @Pointcut("@annotation(Deprecated)")
-//    public void withinPointCut() {
-//    }
-//
-//    @Before("withinPointCut()")
-//    public void withinBefore() {
-//        System.out.println("********** BEFORE ADVICE ************");
-//    }
-
-    @Pointcut("@args(org.springframework.stereotype.Component)")
+//    @Pointcut("bean(lens)")
+    @Pointcut("bean(*e*)")
     public void withinPointCut() {
     }
 
